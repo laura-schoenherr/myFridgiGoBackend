97ad84387ce8603187ec34ee697c07b1d12310ca
laura-schoenherr <lauraschoenherr@outlook.de>
2021-11-16 11:54:44 +0100

    Initial commit

---
+++ src/main/java/com/example/myfridgigobackend/MyFridgiGoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfridgigobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFridgiGoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFridgiGoBackendApplication.class, args);
+    }
+
+}
